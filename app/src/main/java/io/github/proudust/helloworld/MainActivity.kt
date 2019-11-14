db8be00550ab5116c7f4616fceccd6e7432a828d
Proudust <proudust@gmail.com>
2019-11-14 10:32:37 +0900

    :art: 生成されたコードの再フォーマット

--- app/src/main/java/io/github/proudust/helloworld/MainActivity.kt
+++ app/src/main/java/io/github/proudust/helloworld/MainActivity.kt
@@ -1,7 +1,7 @@
 package io.github.proudust.helloworld
 
-import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import androidx.appcompat.app.AppCompatActivity
 import io.github.proudust.helloworld.ui.main.MainFragment
 
 class MainActivity : AppCompatActivity() {
--- app/src/main/java/io/github/proudust/helloworld/ui/main/MainFragment.kt
+++ app/src/main/java/io/github/proudust/helloworld/ui/main/MainFragment.kt
@@ -1,11 +1,11 @@
 package io.github.proudust.helloworld.ui.main
 
-import androidx.lifecycle.ViewModelProviders
 import android.os.Bundle
-import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import androidx.fragment.app.Fragment
+import androidx.lifecycle.ViewModelProviders
 import io.github.proudust.helloworld.R
 
 class MainFragment : Fragment() {
