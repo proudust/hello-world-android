188f74e815b46db7fa0eda96b00048b7eceaabe5
Proudust <proudust@gmail.com>
2019-11-15 16:59:27 +0900

    :recycle: Activity と Fragment のレイアウト ID をコンストラクタから渡すように変更

--- app/src/main/java/io/github/proudust/helloworld/ui/main/MainFragment.kt
+++ app/src/main/java/io/github/proudust/helloworld/ui/main/MainFragment.kt
@@ -1,23 +1,13 @@
 package io.github.proudust.helloworld.ui.main
 
 import android.os.Bundle
-import android.view.LayoutInflater
-import android.view.View
-import android.view.ViewGroup
 import androidx.fragment.app.Fragment
 import io.github.proudust.helloworld.R
 import org.koin.android.ext.android.inject
 
-class MainFragment : Fragment() {
+class MainFragment : Fragment(R.layout.main_fragment) {
 
     private val viewModel: MainViewModel by inject()
-
-    override fun onCreateView(
-        inflater: LayoutInflater, container: ViewGroup?,
-        savedInstanceState: Bundle?
-    ): View {
-        return inflater.inflate(R.layout.main_fragment, container, false)
-    }
 
     override fun onActivityCreated(savedInstanceState: Bundle?) {
         super.onActivityCreated(savedInstanceState)
