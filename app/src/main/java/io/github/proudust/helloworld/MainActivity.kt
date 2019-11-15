318375f7de9153889246d37a1deb891129b3d541
Proudust <proudust@gmail.com>
2019-11-15 15:25:19 +0900

    :recycle: MainActivity の画面遷移に Navigation を用いるように変更

--- app/src/main/java/io/github/proudust/helloworld/MainActivity.kt
+++ app/src/main/java/io/github/proudust/helloworld/MainActivity.kt
@@ -2,18 +2,12 @@ package io.github.proudust.helloworld
 
 import android.os.Bundle
 import androidx.appcompat.app.AppCompatActivity
-import io.github.proudust.helloworld.ui.main.MainFragment
 
 class MainActivity : AppCompatActivity() {
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.main_activity)
-        if (savedInstanceState == null) {
-            supportFragmentManager.beginTransaction()
-                .replace(R.id.container, MainFragment.newInstance())
-                .commitNow()
-        }
     }
 
 }
